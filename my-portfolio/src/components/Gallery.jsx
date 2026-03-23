8fd4819626868635fa26e38e8a1fe3548e9f74c0
Akilach01 <akilapeiris92@gmail.com>
2026-03-23 16:13:07 +0530

    added gallery section

---
+++ my-portfolio/src/components/Gallery.jsx
@@ -0,0 +1,21 @@
+function Gallery() {
+  return (
+    <section id="gallery">
+      <h1>Gallery</h1>
+
+      <div className="gallery-item1">
+        <h2>AI Arts 🤖</h2>
+
+        <img src="/assets/images/tiger closeup AI.jpg" />
+      </div>
+
+      <div className="gallery-item2">
+        <h2>Mobile Photography 📸</h2>
+
+        <img src="/assets/images/epicia 20230704_181639.jpg" />
+      </div>
+    </section>
+  );
+}
+
+export default Gallery;
