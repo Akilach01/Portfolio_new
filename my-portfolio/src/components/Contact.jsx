f405e81c7e66d3cba2b44eee7cf0ed80ebedcab7
Akilach01 <akilapeiris92@gmail.com>
2026-03-23 16:13:24 +0530

    added contact section

---
+++ my-portfolio/src/components/Contact.jsx
@@ -0,0 +1,23 @@
+function Contact() {
+  return (
+    <section id="contact">
+      <div className="contact-info">
+        <h1>Contact</h1>
+
+        <p>Email: akilapeiris92@gmail.com</p>
+        <p>Phone: +94 71 586 74 57</p>
+      </div>
+
+      <div className="contact-form">
+        <form>
+          <input type="text" placeholder="Name" required />
+          <input type="email" placeholder="Email" required />
+          <textarea placeholder="Message" required></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
