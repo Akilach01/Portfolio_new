f7db0828ceb6bfab0efb4da9dc046ad33b36590e
Akilach01 <akilapeiris92@gmail.com>
2026-03-23 16:09:55 +0530

    created home section

---
+++ my-portfolio/src/components/Home.jsx
@@ -0,0 +1,46 @@
+function Home() {
+  return (
+    <section id="home">
+      <div className="left">
+        <h1>👋|Hello it's me</h1>
+        <h2>Akila <span> chamara </span>!</h2>
+
+        <p>
+          A passionate software developer and problem-solver.
+          <br />
+          "I turn ideas into reality with code". keep scrolling to see my work.
+        </p>
+
+        <div className="social-media-links">
+          <a href="https://www.linkedin.com/" target="_blank">
+            <img src="https://img.icons8.com/?size=100&id=xuvGCOXi8Wyg&format=png" />
+          </a>
+
+          <a href="https://github.com/" target="_blank">
+            <img src="https://img.icons8.com/?size=100&id=AZOZNnY73haj&format=png" />
+          </a>
+        </div>
+
+        <div id="download">
+          <a href="#">
+            <button>Download CV</button>
+          </a>
+        </div>
+      </div>
+
+      <div className="right">
+        <img
+          className="profilepic"
+          src="/assets/images/my photo blue & red.jpg"
+          alt="profile"
+        />
+
+        <a href="#about">
+          <button>More about me ⇒</button>
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
